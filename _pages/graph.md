946ab15ad371cf82bfa52edbd999a43018883f4c
Richard Pordan <105643799+richardpordan@users.noreply.github.com>
2025-11-03 20:20:43 +0000

    fix urls more

--- _pages/graph.md
+++ _pages/graph.md
@@ -5,4 +5,4 @@ title: Graph
 
 <svg id="graph"></svg>
 <script src="{{ site.data.vars.d3_cdn }}"></script>
-<script src="{{ site.data.vars.graph_js_path | relative_url }}"></script>
+<script src="{{ site.data.vars.graph_js_path | prepend: site.url }}"></script>
--- assets/js/graph.js
+++ assets/js/graph.js
@@ -1,5 +1,7 @@
 async function draw_graph() {
-    const graph = await d3.json("/assets/data/graph.json")
+    const graph = await d3.json(
+      "../assets/data/graph.json"
+    )
     
     const svgParent = document.getElementById("content-container")
     const width = svgParent.clientWidth
